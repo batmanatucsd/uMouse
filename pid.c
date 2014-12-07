4c7812aa558da7dc3f04446c8638aa7b40c525bf
alvinheng <a1heng@ucsd.edu>
2014-12-06 18:53:33 -0800

    compiles

--- pid.c
+++ pid.c
@@ -2,4 +2,30 @@
 
 void pid()
 {
+	// TODO
+	// Initialize leftIR and rightIR
+
+	if ((leftIR >= THRESHOLD && rightIR <= THRESHOLD) ||
+		(leftIR <= THRESHOLD && rightIR >= THRESHOLD))
+	{
+		currentError = leftIR - rightIR;
+	} else if (leftIR < THRESHOLD)
+	{
+		currentError =  THRESHOLD - rightIR;
+	} else if (rightIR < THRESHOLD)
+	{
+		currentError = leftIR - THRESHOLD;
+	} else if (leftIR < THRESHOLD && rightIR < THRESHOLD)
+	{
+		currentError = (rightEncoder - leftEncoder); // * some constant
+	}
+
+	float proportional = currentError;
+	float derivative = currentError - pastError;
+	float total = KP * currentError + KD * derivative;
+
+	leftSpeed += total / ADJUST;
+	rightSpeed -= total / ADJUST;
+
+	pastError = currentError;
 }
--- pid.h
+++ pid.h
@@ -1,7 +1,9 @@
+// Constants
+float THRESHOLD, KP, KD, ADJUST;
+
 // Global variables
 float pastError, currentError, leftIR, rightIR, leftEncoder, rightEncoder, leftSpeed, rightSpeed;
 
-// Depends on how precise or memory-limited we are
 // double pastError, currentError, leftIR, rightIR, leftEncoder, rightEncoder, leftSpeed, rightSpeed;
 
 void pid();
