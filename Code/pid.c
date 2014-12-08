783d6749cd281b2801e15d2ee306e6c527020d48
James Lee <batman@Jamess-MacBook-Pro.local>
2014-12-07 20:54:21 -0800

    updated pid.c

--- Code/pid.c
+++ Code/pid.c
@@ -6,23 +6,20 @@ void pid()
 	// Initialize leftIR and rightIR
 
 	if ((leftIR >= THRESHOLD && rightIR <= THRESHOLD) ||
-		(leftIR <= THRESHOLD && rightIR >= THRESHOLD))
-	{
+		(leftIR <= THRESHOLD && rightIR >= THRESHOLD)) {
 		currentError = leftIR - rightIR;
-	} else if (leftIR < THRESHOLD)
-	{
+	} 
+  else if (leftIR < THRESHOLD) {
 		currentError =  THRESHOLD - rightIR;
-	} else if (rightIR < THRESHOLD)
-	{
+	} 
+  else if (rightIR < THRESHOLD) {
 		currentError = leftIR - THRESHOLD;
-	} else if (leftIR < THRESHOLD && rightIR < THRESHOLD)
-	{
+	}
+  else if (leftIR < THRESHOLD && rightIR < THRESHOLD) {
 		currentError = (rightEncoder - leftEncoder); // * some constant
 	}
 
-	float proportional = currentError;
-	float derivative = currentError - pastError;
-	float total = KP * currentError + KD * derivative;
+	float total = KP * currentError + KD * (currentError - pastError);
 
 	leftSpeed += total / ADJUST;
 	rightSpeed -= total / ADJUST;
