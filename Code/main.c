acf24dd4bd988fc82b5d6740f4c335d6b818d5bb
Yiyu Zhu <smallzzy@outlook.com>
2014-12-16 04:26:16 +0800

    gitignore & optimize level update

--- Code/main.c
+++ Code/main.c
@@ -1,10 +1,22 @@
 #include <stm32f10x.h>
 
+GPIO_InitTypeDef GPIO_InitStructure;
+
 int main(void)
 {
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+	
+	/* Configure PD0 and PD2 in output pushpull mode */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
 	while(1)
 	{
-
+		/* Set PD0 and PD2 */
+		GPIOD->BSRR = 0x00000005;
+		/* Reset PD0 and PD2 */
+		GPIOD->BRR  = 0x00000005;
 	}
 	return 0;
 }
