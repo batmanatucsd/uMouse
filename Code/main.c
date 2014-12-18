509bd0fe11ab8fa41d90ac009f6003257e9892b3
Yiyu Zhu <smallzzy@outlook.com>
2014-12-19 04:00:12 +0800

    confirm bug in the building system

--- Code/main.c
+++ Code/main.c
@@ -1,22 +1,39 @@
 #include <stm32f10x.h>
-
-GPIO_InitTypeDef GPIO_InitStructure;
+void delay(int);
 
 int main(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 	
-	/* Configure PD0 and PD2 in output pushpull mode */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
 	while(1)
 	{
-		/* Set PD0 and PD2 */
-		GPIOD->BSRR = 0x00000005;
-		/* Reset PD0 and PD2 */
-		GPIOD->BRR  = 0x00000005;
-	}
+		//GPIO_SetBits(GPIOC, GPIO_Pin_8);
+		//delay(10000);
+		//GPIO_ResetBits(GPIOC, GPIO_Pin_8);
+		//delay(10000);
+
+		delay(1000000);
+
+		GPIO_ResetBits(GPIOC, GPIO_Pin_9);
+		delay(1000000);
+
+
+		GPIO_SetBits(GPIOC, GPIO_Pin_9);
+		delay(1000000);
+			}
 	return 0;
+}
+
+
+
+
+void delay(int i)
+{
+	while(i--);
 }
