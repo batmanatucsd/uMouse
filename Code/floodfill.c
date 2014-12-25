0c6df3e554a4d1ccaefc2a9a76fa74d487143e96
alvinheng <a1heng@ucsd.edu>
2014-12-24 22:26:32 -0800

    floodfill in progress

--- Code/floodfill.c
+++ Code/floodfill.c
@@ -1,24 +1,62 @@
+#include <stdio.h>
+
 #include "floodfill.h"
 
-void init() {
+void setup() {
+
+	// Initialize the board
 	for (unsigned short row = 0; row < 16; row++) {
 		for (unsigned short col = 0; col < 16; col++) {
-			board[row][col] = calc(row, col);
+			board[row][col] = init(row, col);
 		}
+	}
+	// Alternative for loop
+	/*  for (unsigned short cell = 0; cell < 256; cell++) {
+			 board[cell / 16][cell % 16] = init(cell / 16, cell % 16);
+		}
+	*/
+}
+
+unsigned short init(unsigned short row, unsigned short col) {
+	
+	if(row > 0x07) {
+		row = 0x07 - (row - 0x08);
+	}
+	
+	if(col > 0x07) {
+		col = 0x07 - (col - 0x08);
+	}
+
+	return 0x0e - col - row;
+}
+
+
+void update(unsigned short row, unsigned short col) {
+	unsigned short cell = stack[--stackptr];
+	// Update 4 walls and neighbors
+}
+
+
+void print() {
+
+	for (unsigned short row = 0; row < 16; row++) {
+		for (unsigned short col = 0; col < 16; col++) {
+			// TODO
+			// Print walls and posts
+			printf("%d\t", board[row][col] & VALUE);
+		}
+		printf("\n");
 	}
 }
 
-unsigned short calc(unsigned short row1, unsigned short col1) {
-	
-	if(row1>0x08) {
-		nr = row1 – 0x09;
-		row1 = 0x08 – nr;
-	}
-	
-	if(col1>0x08) {
-		nc = col1 – 0x09;
-		col1 = 0x08 – nc;
-	}
+int main() {
+	setup();
 
-	return(0x0f – (col1-0x01) – (row1 – 0x01));
+	// Push first cell into stack
+	stack[stackptr++] = board[15][0];
+	print();
+
+	while (stackptr > 0) {
+		update(15, 0);
+	}
 }
--- Code/floodfill.h
+++ Code/floodfill.h
@@ -1,5 +1,21 @@
 unsigned short board[16][16];
 
-void init();
+// Bit masks
+unsigned short VISITED = 0x1000;
+unsigned short NORTH_WALL = 0x0800;
+unsigned short EAST_WALL = 0x0400;
+unsigned short SOUTH_WALL = 0x0200;
+unsigned short WEST_WALL = 0x0100;
+unsigned short VALUE = 0xff;
 
-unsigned short calc(unsigned short row1, unsigned short col1);
+// DFS stack
+unsigned short stack[512];
+unsigned short stackptr = 0;
+
+void setup();
+
+unsigned short init(unsigned short row, unsigned short col);
+
+void update(unsigned short row, unsigned short col);
+
+void print();
