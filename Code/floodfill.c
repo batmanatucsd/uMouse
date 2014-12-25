4f7d5b0fb48b4ee9c33d46203e2cb4aa650ff75d
alvinheng <a1heng@ucsd.edu>
2014-12-25 09:30:50 -0800

    print maze

--- Code/floodfill.c
+++ Code/floodfill.c
@@ -8,13 +8,12 @@ void setup() {
 	for (unsigned short row = 0; row < 16; row++) {
 		for (unsigned short col = 0; col < 16; col++) {
 			board[row][col] = init(row, col);
+			if (row == 0) board[row][col] |= NORTH_WALL;
+			if (col == 0) board[row][col] |= WEST_WALL;
+			if (col == 15) board[row][col] |= EAST_WALL;
+			if (row == 15) board[row][col] |= SOUTH_WALL;
 		}
 	}
-	// Alternative for loop
-	/*  for (unsigned short cell = 0; cell < 256; cell++) {
-			 board[cell / 16][cell % 16] = init(cell / 16, cell % 16);
-		}
-	*/
 }
 
 unsigned short init(unsigned short row, unsigned short col) {
@@ -32,7 +31,9 @@ unsigned short init(unsigned short row, unsigned short col) {
 
 
 void update(unsigned short row, unsigned short col) {
+	
 	unsigned short cell = stack[--stackptr];
+	
 	// Update 4 walls and neighbors
 }
 
@@ -41,12 +42,23 @@ void print() {
 
 	for (unsigned short row = 0; row < 16; row++) {
 		for (unsigned short col = 0; col < 16; col++) {
-			// TODO
-			// Print walls and posts
-			printf("%d\t", board[row][col] & VALUE);
+			printf("+%s", board[row][col] & NORTH_WALL?
+				"---": "   ");
 		}
-		printf("\n");
+		printf("+\n");
+		for (unsigned short col = 0; col < 16; col++) {
+			printf("%s%3d", board[row][col] & WEST_WALL?
+				"|": " ", board[row][col] & VALUE);
+			// TODO Print the visited cells and mouse location and direction
+		}
+		printf("%s\n", board[row][15] & EAST_WALL?
+			"|": " ");
 	}
+	for (unsigned short col = 0; col < 16; col++) {
+		printf("+%s", board[15][col] & SOUTH_WALL?
+			"---": "   ");
+	}
+	printf("+\n");
 }
 
 int main() {
@@ -55,8 +67,10 @@ int main() {
 	// Push first cell into stack
 	stack[stackptr++] = board[15][0];
 	print();
-
+/*
+	//while (dist > 0)
 	while (stackptr > 0) {
 		update(15, 0);
 	}
+*/
 }
