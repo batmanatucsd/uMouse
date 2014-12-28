f60ec502c2a171fdb71279fc5b055f496fde2fdc
James Lee <batman@Jamess-MacBook-Pro.local>
2014-12-28 11:18:35 -0600

    changed clock frequency in main.c

--- Code/main.c
+++ Code/main.c
@@ -8,7 +8,7 @@ int main(void)
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 	
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
 	while(1)
