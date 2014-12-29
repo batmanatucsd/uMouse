d4eab2a5a2b3a2118ef9146f8b4b361a113f3307
James Lee <batmanatucsd@gmail.com>
2014-12-28 23:20:32 -0600

    factor mcu codes

---
+++ Code/mcu_lib.c
@@ -0,0 +1,85 @@
+#include "mcu_lib.h"
+
+void GPIO_Configuration(void) 
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+}
+
+void USART_Configuration(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE); 
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);  
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	USART_InitTypeDef USART_InitStructure;
+
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+	USART_Init(USART1, &USART_InitStructure);
+
+	USART_Cmd(USART1, ENABLE);
+}
+
+// void ADC_Configuration(void)
+// {
+//   ADC_InitTypeDef  ADC_InitStructure;
+//   /* PCLK2 is the APB2 clock */
+//   /* ADCCLK = PCLK2/6 = 72/6 = 12MHz*/
+//   RCC_ADCCLKConfig(RCC_PCLK2_Div6);
+
+//   /* Enable ADC1 clock so that we can talk to it */
+//   RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+//   /* Put everything back to power-on defaults */
+//   ADC_DeInit(ADC1);
+
+//   /* ADC1 Configuration ------------------------------------------------------*/
+//   /* ADC1 and ADC2 operate independently */
+//   ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+//   /* Disable the scan conversion so we do one at a time */
+//   ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+//   /* Don't do contimuous conversions - do them on demand */
+//   ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+//   // Start conversin by software, not an external trigger 
+//   ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+//   /* Conversions are 12 bit - put them in the lower 12 bits of the result */
+//   ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+//   /* Say how many channels would be used by the sequencer */
+//   ADC_InitStructure.ADC_NbrOfChannel = 1;
+
+//   /* Now do the setup */
+//   ADC_Init(ADC1, &ADC_InitStructure);
+//   /* Enable ADC1 */
+//   ADC_Cmd(ADC1, ENABLE);
+
+//   /* Enable ADC1 reset calibaration register */
+//   ADC_ResetCalibration(ADC1);
+//   /* Check the end of ADC1 reset calibration register */
+//   while(ADC_GetResetCalibrationStatus(ADC1));
+//   /* Start ADC1 calibaration */
+//   ADC_StartCalibration(ADC1);
+//   /* Check the end of ADC1 calibration */
+//   while(ADC_GetCalibrationStatus(ADC1));
+// }
+
---
+++ Code/mcu_lib.h
@@ -0,0 +1,10 @@
+#ifndef _MCU_LIB_H
+#define _MCU_LIB_H
+
+#include <stm32f10x.h>
+
+void GPIO_Configuration(void);
+void USART_Configuration(void);
+void ADC_Configuration(void);
+
+#endif // _MCU_LIB_H
