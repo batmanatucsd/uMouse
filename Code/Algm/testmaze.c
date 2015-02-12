785935b7afb7bcc0d2c5bab8cdbe801ef916c01e
tk21 <teresado.21t@gmail.com>
2015-02-11 22:20:04 -0800

    Makefile to include testmaze stuff.

--- Code/Algm/testmaze.c
+++ Code/Algm/testmaze.c
@@ -1,122 +1,300 @@
-/*****************************************************************************/
-// CREATES TEST MAZES
-//		The wall information will be hardcoded for now....
-//		Walls are going to be stored in TILE 
-//		
-/*****************************************************************************/
+/****************************************************************************/
+// testMaze.h
+//		This actually 
+//
+/****************************************************************************/
+
 #include <stdio.h>
+#include <stdlib.h>
 #include "floodfill.h"
-#include "testmaze.h"
 
+unsigned short testMaze[16][16];		//This represents the actual testMaze, 
+										//mouse does not know this yet.
 
-/*****************************************************************************/
-// setup()
-// 		Initializes an empty board
-/*****************************************************************************/
-void setup() {
+/****************************************************************************/
+// setupTest():
+//		This actually just hardcodes the maze... lmao
+//		Moves through columns from 0 -> 15, left to right. 
+//		Then for each column, rows 0->15, top to bottom. 
+/****************************************************************************/
+void setupTest() {
 
-	// Initialize, empty 
+	//Initialize edge cells
 	for (unsigned short row = 0; row < 16; row++) {
 		for (unsigned short col = 0; col < 16; col++) {
-			board[row][col] = init(row, col);
-			if (row == 0) board[row][col] |= NORTH_WALL;
-			if (col == 0) board[row][col] |= WEST_WALL;
-			if (col == 15) board[row][col] |= EAST_WALL;
-			if (row == 15) board[row][col] |= SOUTH_WALL;
+
+			//Call init from floodfill.c
+			//Pre-fills cells with distance from center, assuming no walls
+			testMaze[row][col] = init(row, col);
+
+			if (row == 0) testMaze[row][col] |= NORTH_WALL;
+			if (col == 0) testMaze[row][col] |= WEST_WALL;
+			if (col == 15) testMaze[row][col] |= EAST_WALL;
+			if (row == 15) testMaze[row][col] |= SOUTH_WALL;
 		}
 	}
 
 	// Initialize mouse, position in bottom left corner, facing up
-	location = 0xf0;				// board[15,0]
-	direction = 0x0;				// 0x0 = up direction
+	location = 0xf0;					// testMaze[15,0]
+	direction = 0x0;					// 0x0 = up direction
 
-	// Initialize the right wall for the bottom left corner
-	board[15][0] = board[15][0] | WEST_WALL;
+	// Initialize the right wall for the bottom left corner, always
+	testMaze[15][0] |= EAST_WALL;
 
-
-}
-
-/*****************************************************************************/
-// init(unsigned short row, unsigned short col)
-// 		Pre-fill cell with values that determine its distance from the center.
-// 		Initially assumes a wall-less maze.
-/*****************************************************************************/
-unsigned short init(unsigned short row, unsigned short col) {
-
-	//For bottom half of maze, distance from center is the same as top half
-	if(row > 0x07) {
-		row = 0x07 - (row - 0x08);
+	//Open text file to read walls in, read-only
+	FILE *file = fopen("testmaze1.txt", "r");
+	if (!file) {
+		printf("Sorry, that test maze file does not exist.\n");
+		exit(1);
 	}
 
-	//For right half of maze, distance from center is the same as left half
-	if(col > 0x07) {
-		col = 0x07 - (col - 0x08);
+	//Read the text file to create maze
+	while(!feof(file)) {
+		printf("yay we made it in\n");
+		i++;
 	}
 
-	return 0x0e - col - row;
 }
-/*****************************************************************************/
-// print():
-// 		Print out the cell, with the mouse location designated with a carrot
-// 		sign pointing in 
-/*****************************************************************************/
-void print() {
-
-	for (unsigned short row = 0; row < 16; row++) {
-
-		// North wall
-		for (unsigned short col = 0; col < 16; col++) {
-			printf("+%s", board[row][col] & NORTH_WALL?
-				"---": "   ");
+	//Loop through text file to create the maze.
+	/**unsigned short c = 0;
+	for (int row = 0; row < 16; row++) {
+		for (int col = 0; col < 16; col++) {
+			testMaze[row][col] = c;
 		}
-		printf("+\n");
-
-		for (unsigned short col = 0; col < 16; col++) {
-
-			printf("%s", board[row][col] & WEST_WALL?
-				"|": " ");
-
-			// Location direction
-			if (row == (location & ROW) >> 4 &&
-				col == (location & COL)) {
-				switch (direction) {
-					case 0x0: printf("^");
-								break;
-					case 0x1: printf(">");
-								break;
-					case 0x2: printf("v");
-								break;
-					case 0x3: printf("<");
-				}
-			} else if (board[row][col] & VISITED) {
-				printf("*");
-			} else {
-				printf(" ");
-			}
-
-			// Print 2 digits
-			printf("%2d", (board[row][col] & DIST) % 100);
-		}
-
-		printf("%s\n", board[row][15] & EAST_WALL?
-			"|": " ");
-	}
-
-	// South wall
-	for (unsigned short col = 0; col < 16; col++) {
-		printf("+%s", board[15][col] & SOUTH_WALL?
-			"---": "   ");
-	}
-	printf("+\n");
-}
+	} **/
 
 
-int main () {
+/**	//Column 0 WALLS:
+	testMaze[14][0] |= 
+	testMaze[13][0] |= EAST_WALL;
+	testMaze[12][0] |= EAST_WALL;
+	testMaze[11][0] |= NORTH_WALL;
+	testMaze[10][0] |= (SOUTH_WALL | NORTH_WALL);
+	testMaze[9][0] |= (SOUTH_WALL | NORTH_WALL);
+	testMaze[8][0] |= SOUTH_WALL;
+	//testMaze[7][0] |= 
+	testMaze[6][0] |= EAST_WALL;
+	//testMaze[5][0] |= 
+	testMaze[4][0] |= EAST_WALL;
+	//testMaze[3][0] |= 
+	testMaze[2][0] |= EAST_WALL;
+	testMaze[1][0] |= EAST_WALL; 
+	//testMaze[0][0] |=
 
-	// Initialize the right wall for the bottom left corner
-	board[15][0] = board[15][0] | WEST_WALL;
-	print();
-}
+	//Column 1 WALLS: --------------------------------------------------------
+	testMaze[15][1] |= WEST_WALL;
+	testMaze[14][1] |= EAST_WALL;
+	testMaze[13][1] |= WEST_WALL;
+	testMaze[12][1] |= (NORTH_WALL | WEST_WALL);
+	testMaze[11][1] |= (SOUTH_WALL | NORTH_WALL);
+	testMaze[10][1] |= (SOUTH_WALL | NORTH_WALL);
+	testMaze[9][1] |= (SOUTH_WALL | NORTH_WALL);
+	testMaze[8][1] |= (SOUTH_WALL | NORTH_WALL);
+	testMaze[7][1] |= (SOUTH_WALL | EAST_WALL);
+	testMaze[6][1] |= (NORTH_WALL | WEST_WALL);
+	testMaze[5][1] |= (SOUTH_WALL | EAST_WALL);
+	testMaze[4][1] |= (NORTH_WALL | WEST_WALL);
+	testMaze[3][1] |= (SOUTH_WALL | EAST_WALL);
+	testMaze[2][1] |= (NORTH_WALL | WEST_WALL);
+	testMaze[1][1] |= (NORTH_WALL | EAST_WALL | SOUTH_WALL | WEST_WALL); 
+	testMaze[0][1] |= SOUTH_WALL;
+
+	//Column 2 WALLS: --------------------------------------------------------
+	testMaze[15][2] |= NORTH_WALL;
+	testMaze[14][2] |= (NORTH_WALL | SOUTH_WALL | WEST_WALL);
+	testMaze[13][2] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[12][2] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[11][2] |= SOUTH_WALL;
+	testMaze[10][2] |= NORTH_WALL;
+	testMaze[9][2] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[8][2] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[7][2] |= (NORTH_WALL | EAST_WALL | SOUTH_WALL | WEST_WALL); 
+	testMaze[6][2] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[5][2] |= (NORTH_WALL | WEST_WALL);
+	testMaze[4][2] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[3][2] |= (NORTH_WALL | WEST_WALL);
+	testMaze[2][2] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[1][2] |= (NORTH_WALL | WEST_WALL);
+	testMaze[0][2] |= SOUTH_WALL;
+
+	//Column 3 WALLS: --------------------------------------------------------
+	testMaze[15][3] |= NORTH_WALL;
+	testMaze[14][3] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[13][3] |= SOUTH_WALL;
+	testMaze[12][3] |= (NORTH_WALL | EAST_WALL);
+	testMaze[11][3] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[10][3] |= SOUTH_WALL;
+	testMaze[9][3] |= NORTH_WALL;
+	testMaze[8][3] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[7][3] |= (EAST_WALL | WEST_WALL);
+	testMaze[6][3] |= (NORTH_WALL | WEST_WALL);
+	testMaze[5][3] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[4][3] |= (NORTH_WALL | WEST_WALL);
+	testMaze[3][3] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[2][3] |= (NORTH_WALL | WEST_WALL);
+	testMaze[1][3] |= (EAST_WALL | SOUTH_WALL);
+	//testMaze[0][3] |=
+
+	//Column 4 WALLS: --------------------------------------------------------
+	testMaze[15][4] |= NORTH_WALL;
+	testMaze[14][4] |= SOUTH_WALL;
+	testMaze[13][4] |= EAST_WALL;
+	testMaze[12][4] |= WEST_WALL;
+	testMaze[11][4] |= NORTH_WALL;
+	testMaze[10][4] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[9][4] |= SOUTH_WALL;
+	testMaze[8][4] |= (EAST_WALL | WEST_WALL);
+	testMaze[7][4] |= (NORTH_WALL | WEST_WALL);
+	testMaze[6][4] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[5][4] |= (NORTH_WALL | EAST_WALL | SOUTH_WALL | WEST_WALL); 
+	testMaze[4][4] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[3][4] |= (NORTH_WALL | WEST_WALL);
+	testMaze[2][4] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[1][4] |= (NORTH_WALL | WEST_WALL);
+	testMaze[0][4] |= SOUTH_WALL;
+
+	//Column 5 WALLS: --------------------------------------------------------
+	testMaze[15][5] |= NORTH_WALL;
+	testMaze[14][5] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[13][5] |= (NORTH_WALL | EAST_WALL | SOUTH_WALL | WEST_WALL);
+	testMaze[12][5] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[11][5] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[10][5] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[9][5] |= SOUTH_WALL;
+	testMaze[8][5] |= (EAST_WALL | WEST_WALL);
+	testMaze[7][5] |= (NORTH_WALL | WEST_WALL);
+	testMaze[6][5] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[5][5] |= (EAST_WALL | WEST_WALL);
+	testMaze[4][5] |= (NORTH_WALL | WEST_WALL);
+	testMaze[3][5] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[2][5] |= (NORTH_WALL | SOUTH_WALL | WEST_WALL);
+	testMaze[1][5] |= (EAST_WALL | SOUTH_WALL);
+	//testMaze[0][5] |=
+
+	//Column 6 WALLS: --------------------------------------------------------
+	testMaze[15][6] |= NORTH_WALL;
+	testMaze[14][6] |= SOUTH_WALL;
+	testMaze[13][6] |= EAST_WALL | WEST_WALL;
+	testMaze[12][6] |= NORTH_WALL;
+	testMaze[11][6] |= EAST_WALL | SOUTH_WALL;
+	testMaze[10][6] |= NORTH_WALL;
+	testMaze[9][6] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[8][6] |= SOUTH_WALL | WEST_WALL;
+	testMaze[7][6] |= EAST_WALL | WEST_WALL;
+	testMaze[6][6] |= WEST_WALL;
+	testMaze[5][6] |= EAST_WALL | WEST_WALL;
+	testMaze[4][6] |= EAST_WALL;
+	testMaze[3][6] |= NORTH_WALL | EAST_WALL;
+	testMaze[2][6] |= SOUTH_WALL;
+	testMaze[1][6] |= (EAST_WALL | WEST_WALL);
+	//testMaze[0][6] |=
+
+	//Column 7 WALLS:
+	testMaze[15][7] |= NORTH_WALL;
+	testMaze[14][7] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][7] |=
+	testMaze[12][7] |=
+	testMaze[11][7] |=
+	testMaze[10][7] |=
+	testMaze[9][7] |=
+	testMaze[8][7] |=
+	testMaze[7][7] |=
+	testMaze[6][7] |=
+	testMaze[5][7] |=
+	testMaze[4][7] |=
+	testMaze[3][7] |=
+	testMaze[2][7] |=
+	testMaze[1][7] |=
+	testMaze[0][7] |= SOUTH_WALL;
+
+	//Column 8 WALLS: --------------------------------------------------------
+	testMaze[15][8] |= NORTH_WALL;
+	testMaze[14][8] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][8] |=
+	testMaze[12][8] |=
+	testMaze[11][8] |=
+	testMaze[10][8] |=
+	testMaze[9][8] |=
+	testMaze[8][8] |=
+	testMaze[7][8] |=
+	testMaze[6][8] |=
+	testMaze[5][8] |=
+	testMaze[4][8] |=
+	testMaze[3][8] |=
+	testMaze[2][8] |=
+	testMaze[1][8] |=
+	//testMaze[0][8] |=
+
+	//Column 9 WALLS: --------------------------------------------------------
+	testMaze[15][9] |= NORTH_WALL;
+	testMaze[14][9] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][9] |=
+	testMaze[12][9] |=
+	testMaze[11][9] |=
+	testMaze[10][9] |=
+	testMaze[9][9] |=
+	testMaze[8][9] |=
+	testMaze[7][9] |=
+	testMaze[6][9] |=
+	testMaze[5][9] |=
+	testMaze[4][9] |=
+	testMaze[3][9] |=
+	testMaze[2][9] |=
+	testMaze[1][9] |=
+	testMaze[0][9] |= SOUTH_WALL;
+
+	//Column 10 WALLS: --------------------------------------------------------
+	testMaze[15][10] |= NORTH_WALL;
+	testMaze[14][10] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][10] |=
+	testMaze[12][10] |=
+	testMaze[11][10] |=
+	testMaze[10][10] |=
+	testMaze[9][10] |=
+	testMaze[8][10] |=
+	testMaze[7][10] |=
+	testMaze[6][10] |=
+	testMaze[5][10] |=
+	testMaze[4][10] |=
+	testMaze[3][10] |=
+	testMaze[2][10] |=
+	testMaze[1][10] |=
+	testMaze[0][10] |= SOUTH_WALL;
+
+	//Column 11 WALLS: --------------------------------------------------------
+	testMaze[15][11] |= NORTH_WALL;
+	testMaze[14][11] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][11] |=
+	testMaze[12][11] |=
+	testMaze[11][11] |=
+	testMaze[10][11] |=
+	testMaze[9][11] |=
+	testMaze[8][11] |=
+	testMaze[7][11] |=
+	testMaze[6][11] |=
+	testMaze[5][11] |=
+	testMaze[4][11] |=
+	testMaze[3][11] |=
+	testMaze[2][11] |=
+	testMaze[1][11] |=
+
+	//Column 12 WALLS: --------------------------------------------------------
+	testMaze[15][11] |= NORTH_WALL;
+	testMaze[14][11] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][11] |=
+	testMaze[12][11] |=
+	testMaze[11][11] |=
+	testMaze[10][11] |=
+	testMaze[9][11] |=
+	testMaze[8][11] |=
+	testMaze[7][11] |=
+	testMaze[6][11] |=
+	testMaze[5][11] |=
+	testMaze[4][11] |=
+	testMaze[3][11] |=
+	testMaze[2][11] |=
+	testMaze[1][11] |= 
 
 
 
+} **/
--- Code/Algm/testmaze.h
+++ Code/Algm/testmaze.h
@@ -4,22 +4,29 @@
 //
 /****************************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "floodfill.h"
+
 unsigned short testMaze[16][16];		//This represents the actual testMaze, 
 										//mouse does not know this yet.
 
 /****************************************************************************/
 // setupTest():
 //		This actually just hardcodes the maze... lmao
-//		Order of set up will start from bottom left, then going up the row,
-//		then to next column, and going up the row [15 -> 0]
-//		column order will be [0 -> 15]
+//		Moves through columns from 0 -> 15, left to right. 
+//		Then for each column, rows 0->15, top to bottom. 
 /****************************************************************************/
 void setupTest() {
 
-	// Initialize, empty 
+	//Initialize edge cells
 	for (unsigned short row = 0; row < 16; row++) {
 		for (unsigned short col = 0; col < 16; col++) {
+
+			//Call init from floodfill.c
+			//Pre-fills cells with distance from center, assuming no walls
 			testMaze[row][col] = init(row, col);
+
 			if (row == 0) testMaze[row][col] |= NORTH_WALL;
 			if (col == 0) testMaze[row][col] |= WEST_WALL;
 			if (col == 15) testMaze[row][col] |= EAST_WALL;
@@ -28,15 +35,37 @@ void setupTest() {
 	}
 
 	// Initialize mouse, position in bottom left corner, facing up
-	location = 0xf0;				// testMaze[15,0]
-	direction = 0x0;				// 0x0 = up direction
+	location = 0xf0;					// testMaze[15,0]
+	direction = 0x0;					// 0x0 = up direction
 
-
-	// Initialize the right wall for the bottom left corner 
-	// *** THIS WILL ALWAYS BE THE SAME ***
+	// Initialize the right wall for the bottom left corner, always
 	testMaze[15][0] |= EAST_WALL;
 
-	//Column 0 WALLS:
+	//Open text file to read walls in, read-only
+	FILE *file = fopen("testmaze1.txt", "r");
+	if (!file) {
+		printf("Sorry, that test maze file does not exist.\n");
+		exit(1);
+	}
+
+	//Read the text file to create maze
+	int i = 0;
+	while(!feof(file)) {
+		printf("yay we made it in\n");
+		i++;
+	}
+
+}
+	//Loop through text file to create the maze.
+	/**unsigned short c = 0;
+	for (int row = 0; row < 16; row++) {
+		for (int col = 0; col < 16; col++) {
+			testMaze[row][col] = c;
+		}
+	} **/
+
+
+/**	//Column 0 WALLS:
 	testMaze[14][0] |= 
 	testMaze[13][0] |= EAST_WALL;
 	testMaze[12][0] |= EAST_WALL;
@@ -53,7 +82,7 @@ void setupTest() {
 	testMaze[1][0] |= EAST_WALL; 
 	//testMaze[0][0] |=
 
-	//Column 1 WALLS:
+	//Column 1 WALLS: --------------------------------------------------------
 	testMaze[15][1] |= WEST_WALL;
 	testMaze[14][1] |= EAST_WALL;
 	testMaze[13][1] |= WEST_WALL;
@@ -62,16 +91,211 @@ void setupTest() {
 	testMaze[10][1] |= (SOUTH_WALL | NORTH_WALL);
 	testMaze[9][1] |= (SOUTH_WALL | NORTH_WALL);
 	testMaze[8][1] |= (SOUTH_WALL | NORTH_WALL);
-	//testMaze[7][0] |= 
-	testMaze[6][1] |= EAST_WALL;
-	//testMaze[5][0] |= 
-	testMaze[4][1] |= EAST_WALL;
-	//testMaze[3][0] |= 
-	testMaze[2][1] |= EAST_WALL;
-	testMaze[1][1] |= EAST_WALL; 
-	//testMaze[0][0] |=
+	testMaze[7][1] |= (SOUTH_WALL | EAST_WALL);
+	testMaze[6][1] |= (NORTH_WALL | WEST_WALL);
+	testMaze[5][1] |= (SOUTH_WALL | EAST_WALL);
+	testMaze[4][1] |= (NORTH_WALL | WEST_WALL);
+	testMaze[3][1] |= (SOUTH_WALL | EAST_WALL);
+	testMaze[2][1] |= (NORTH_WALL | WEST_WALL);
+	testMaze[1][1] |= (NORTH_WALL | EAST_WALL | SOUTH_WALL | WEST_WALL); 
+	testMaze[0][1] |= SOUTH_WALL;
+
+	//Column 2 WALLS: --------------------------------------------------------
+	testMaze[15][2] |= NORTH_WALL;
+	testMaze[14][2] |= (NORTH_WALL | SOUTH_WALL | WEST_WALL);
+	testMaze[13][2] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[12][2] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[11][2] |= SOUTH_WALL;
+	testMaze[10][2] |= NORTH_WALL;
+	testMaze[9][2] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[8][2] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[7][2] |= (NORTH_WALL | EAST_WALL | SOUTH_WALL | WEST_WALL); 
+	testMaze[6][2] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[5][2] |= (NORTH_WALL | WEST_WALL);
+	testMaze[4][2] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[3][2] |= (NORTH_WALL | WEST_WALL);
+	testMaze[2][2] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[1][2] |= (NORTH_WALL | WEST_WALL);
+	testMaze[0][2] |= SOUTH_WALL;
+
+	//Column 3 WALLS: --------------------------------------------------------
+	testMaze[15][3] |= NORTH_WALL;
+	testMaze[14][3] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[13][3] |= SOUTH_WALL;
+	testMaze[12][3] |= (NORTH_WALL | EAST_WALL);
+	testMaze[11][3] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[10][3] |= SOUTH_WALL;
+	testMaze[9][3] |= NORTH_WALL;
+	testMaze[8][3] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[7][3] |= (EAST_WALL | WEST_WALL);
+	testMaze[6][3] |= (NORTH_WALL | WEST_WALL);
+	testMaze[5][3] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[4][3] |= (NORTH_WALL | WEST_WALL);
+	testMaze[3][3] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[2][3] |= (NORTH_WALL | WEST_WALL);
+	testMaze[1][3] |= (EAST_WALL | SOUTH_WALL);
+	//testMaze[0][3] |=
+
+	//Column 4 WALLS: --------------------------------------------------------
+	testMaze[15][4] |= NORTH_WALL;
+	testMaze[14][4] |= SOUTH_WALL;
+	testMaze[13][4] |= EAST_WALL;
+	testMaze[12][4] |= WEST_WALL;
+	testMaze[11][4] |= NORTH_WALL;
+	testMaze[10][4] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[9][4] |= SOUTH_WALL;
+	testMaze[8][4] |= (EAST_WALL | WEST_WALL);
+	testMaze[7][4] |= (NORTH_WALL | WEST_WALL);
+	testMaze[6][4] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[5][4] |= (NORTH_WALL | EAST_WALL | SOUTH_WALL | WEST_WALL); 
+	testMaze[4][4] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[3][4] |= (NORTH_WALL | WEST_WALL);
+	testMaze[2][4] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[1][4] |= (NORTH_WALL | WEST_WALL);
+	testMaze[0][4] |= SOUTH_WALL;
+
+	//Column 5 WALLS: --------------------------------------------------------
+	testMaze[15][5] |= NORTH_WALL;
+	testMaze[14][5] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[13][5] |= (NORTH_WALL | EAST_WALL | SOUTH_WALL | WEST_WALL);
+	testMaze[12][5] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[11][5] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[10][5] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[9][5] |= SOUTH_WALL;
+	testMaze[8][5] |= (EAST_WALL | WEST_WALL);
+	testMaze[7][5] |= (NORTH_WALL | WEST_WALL);
+	testMaze[6][5] |= (EAST_WALL | SOUTH_WALL);
+	testMaze[5][5] |= (EAST_WALL | WEST_WALL);
+	testMaze[4][5] |= (NORTH_WALL | WEST_WALL);
+	testMaze[3][5] |= (NORTH_WALL | SOUTH_WALL);
+	testMaze[2][5] |= (NORTH_WALL | SOUTH_WALL | WEST_WALL);
+	testMaze[1][5] |= (EAST_WALL | SOUTH_WALL);
+	//testMaze[0][5] |=
+
+	//Column 6 WALLS: --------------------------------------------------------
+	testMaze[15][6] |= NORTH_WALL;
+	testMaze[14][6] |= SOUTH_WALL;
+	testMaze[13][6] |= EAST_WALL | WEST_WALL;
+	testMaze[12][6] |= NORTH_WALL;
+	testMaze[11][6] |= EAST_WALL | SOUTH_WALL;
+	testMaze[10][6] |= NORTH_WALL;
+	testMaze[9][6] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[8][6] |= SOUTH_WALL | WEST_WALL;
+	testMaze[7][6] |= EAST_WALL | WEST_WALL;
+	testMaze[6][6] |= WEST_WALL;
+	testMaze[5][6] |= EAST_WALL | WEST_WALL;
+	testMaze[4][6] |= EAST_WALL;
+	testMaze[3][6] |= NORTH_WALL | EAST_WALL;
+	testMaze[2][6] |= SOUTH_WALL;
+	testMaze[1][6] |= (EAST_WALL | WEST_WALL);
+	//testMaze[0][6] |=
+
+	//Column 7 WALLS:
+	testMaze[15][7] |= NORTH_WALL;
+	testMaze[14][7] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][7] |=
+	testMaze[12][7] |=
+	testMaze[11][7] |=
+	testMaze[10][7] |=
+	testMaze[9][7] |=
+	testMaze[8][7] |=
+	testMaze[7][7] |=
+	testMaze[6][7] |=
+	testMaze[5][7] |=
+	testMaze[4][7] |=
+	testMaze[3][7] |=
+	testMaze[2][7] |=
+	testMaze[1][7] |=
+	testMaze[0][7] |= SOUTH_WALL;
+
+	//Column 8 WALLS: --------------------------------------------------------
+	testMaze[15][8] |= NORTH_WALL;
+	testMaze[14][8] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][8] |=
+	testMaze[12][8] |=
+	testMaze[11][8] |=
+	testMaze[10][8] |=
+	testMaze[9][8] |=
+	testMaze[8][8] |=
+	testMaze[7][8] |=
+	testMaze[6][8] |=
+	testMaze[5][8] |=
+	testMaze[4][8] |=
+	testMaze[3][8] |=
+	testMaze[2][8] |=
+	testMaze[1][8] |=
+	//testMaze[0][8] |=
+
+	//Column 9 WALLS: --------------------------------------------------------
+	testMaze[15][9] |= NORTH_WALL;
+	testMaze[14][9] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][9] |=
+	testMaze[12][9] |=
+	testMaze[11][9] |=
+	testMaze[10][9] |=
+	testMaze[9][9] |=
+	testMaze[8][9] |=
+	testMaze[7][9] |=
+	testMaze[6][9] |=
+	testMaze[5][9] |=
+	testMaze[4][9] |=
+	testMaze[3][9] |=
+	testMaze[2][9] |=
+	testMaze[1][9] |=
+	testMaze[0][9] |= SOUTH_WALL;
+
+	//Column 10 WALLS: --------------------------------------------------------
+	testMaze[15][10] |= NORTH_WALL;
+	testMaze[14][10] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][10] |=
+	testMaze[12][10] |=
+	testMaze[11][10] |=
+	testMaze[10][10] |=
+	testMaze[9][10] |=
+	testMaze[8][10] |=
+	testMaze[7][10] |=
+	testMaze[6][10] |=
+	testMaze[5][10] |=
+	testMaze[4][10] |=
+	testMaze[3][10] |=
+	testMaze[2][10] |=
+	testMaze[1][10] |=
+	testMaze[0][10] |= SOUTH_WALL;
+
+	//Column 11 WALLS: --------------------------------------------------------
+	testMaze[15][11] |= NORTH_WALL;
+	testMaze[14][11] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][11] |=
+	testMaze[12][11] |=
+	testMaze[11][11] |=
+	testMaze[10][11] |=
+	testMaze[9][11] |=
+	testMaze[8][11] |=
+	testMaze[7][11] |=
+	testMaze[6][11] |=
+	testMaze[5][11] |=
+	testMaze[4][11] |=
+	testMaze[3][11] |=
+	testMaze[2][11] |=
+	testMaze[1][11] |=
+
+	//Column 12 WALLS: --------------------------------------------------------
+	testMaze[15][11] |= NORTH_WALL;
+	testMaze[14][11] |= NORTH_WALL | SOUTH_WALL;
+	testMaze[13][11] |=
+	testMaze[12][11] |=
+	testMaze[11][11] |=
+	testMaze[10][11] |=
+	testMaze[9][11] |=
+	testMaze[8][11] |=
+	testMaze[7][11] |=
+	testMaze[6][11] |=
+	testMaze[5][11] |=
+	testMaze[4][11] |=
+	testMaze[3][11] |=
+	testMaze[2][11] |=
+	testMaze[1][11] |= 
 
 
 
-
-}
+} **/
