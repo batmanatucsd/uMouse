fbf6dd900f04e544c8ee9c00139bc20c7b089b68
James Lee <batmanatucsd@gmail.com>
2015-03-26 11:36:58 -0700

    added mouse files from master branch

---
+++ Code/src/mouse.c
@@ -0,0 +1,64 @@
+#include "mouse.h"
+
+/*****************************************************************************/
+// General Functions
+/*****************************************************************************/
+void delay(int i)
+{
+	while(i--);
+}
+
+void listen_for_button(void)
+{
+  if(GPIO_ReadInputDataBit(GPIOB, BUTTON) != Bit_RESET) {
+    state  next_state;
+
+    if(mouse_state == STOP) // if the mouse_state is the last state
+      next_state = GO;      // change it to the first state
+    else 
+      next_state = ++mouse_state; // else next state
+
+    do {
+      mouse_state = next_state;
+    } while(GPIO_ReadInputDataBit(GPIOB, BUTTON) != Bit_RESET);
+  }
+}
+
+/*****************************************************************************/
+// Motor Controls
+/*****************************************************************************/
+void turnMotorOn(void)
+{
+  GPIO_SetBits(MOTOR, STBY);
+
+  // CW
+  GPIO_SetBits(MOTOR, LEFTIN1);
+  GPIO_ResetBits(MOTOR, LEFTIN2);
+
+  // CCW
+  GPIO_ResetBits(MOTOR, RIGHTIN1);
+  GPIO_SetBits(MOTOR, RIGHTIN2);
+}
+
+void turnMotorOff(void)
+{
+  // TODO: should we change the way we turn off the motors?
+  // perhaps turn the inputs off or the pwm (might save battery time)
+  GPIO_WriteBit(MOTOR, STBY, 0);
+}
+
+void change_LeftMotorSpeed(uint16_t speed)
+{
+  // TODO: change to right timer
+  TIM3->CCR3 = speed;
+}
+
+void change_RightMotorSpeed(uint16_t speed)
+{
+  // TODO: change to right timer
+  TIM3->CCR4 = speed;
+}
+
+/*****************************************************************************/
+// Turns
+/*****************************************************************************/
