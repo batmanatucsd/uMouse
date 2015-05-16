532ba1e0b7d96a8a4b035901f94fb0b48100ce1b
Yiyu Zhu <smallzzy@outlook.com>
2015-05-16 06:43:36 -0700

    test 2 angle.c

--- Code/src/angle.c
+++ Code/src/angle.c
@@ -1,4 +1,12 @@
-void MPU6050_SetInitialAngle()
+#include "angle.h"
+#include <math.h>
+
+uint16_t last_count;
+float scaled[6];
+int offset[6] = {0};
+float tangle[6] = {0};
+
+void Angle_SetInitial()
 {
   TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
 
@@ -12,43 +20,58 @@ void MPU6050_SetInitialAngle()
 
   TIM_TimeBaseInit(TIM6, &TIM_TimeBaseStructure);
 
-
   TIM_Cmd(TIM6, ENABLE);
-
-  angle = 0;
-
-  MPU6050_GetZGyro(&last_angle_speed);
-
-  last_count = TIM6 -> CNT;
 }
 
-void MPU6050_UpdateAngle()
+void Angle_ReadRaw()
 {
-  int k = 0.95;
-  float current_angle_speed;
-  MPU6050_GetZGyro(&current_angle_speed);
+  int16_t raw_data[6];
+  MPU6050_GetRawAccelGyro(raw_data);
+
+  scaled[0] = (float)(raw_data[0]-offset[0])*accel_scale_fact/time_scale;
+  scaled[1] = (float)(raw_data[1]-offset[1])*accel_scale_fact/time_scale;
+  scaled[2] = (float)(raw_data[2]-offset[2])*accel_scale_fact/time_scale;
+
+  scaled[3] = (float)(raw_data[3]-offset[3])*gyro_scale_fact/time_scale;
+  scaled[4] = (float)(raw_data[4]-offset[4])*gyro_scale_fact/time_scale;
+  scaled[5] = (float)(raw_data[5]-offset[5])*gyro_scale_fact/time_scale;
+}
+
+float simu_sqrt(float a)
+{
+  double x = 1;
+  int i = 20;
+  while (i--)
+  {
+    x = (x + a / x) /2;
+  }
+  return (float) x;
+}
+
+void MPU6050_UpdateAngle(float* angle)
+{
+  Angle_ReadRaw();
+
   uint16_t current_count = TIM6 ->CNT;
+  uint16_t dt = current_count - last_count;
 
-  float error = current_angle_speed - last_angle_speed;
-  uint16_t t_diff = current_count - last_count;
+  tangle[3] = (scaled[3]*((float)dt/time_scale)+angle[0]);
+  tangle[4] = (scaled[4]*((float)dt/time_scale)+angle[1]);
+  tangle[5] = (scaled[5]*((float)dt/time_scale)+angle[2]);
 
-  angle += k * error * t_diff / 5000;
+  tangle[2] = atan(scaled[2]/(simu_sqrt(scaled[1]*scaled[1]+scaled[0]*scaled[0])))*(float)rad2degree;
+  tangle[1] =-atan(scaled[1]/(simu_sqrt(scaled[1]*scaled[1]+scaled[2]*scaled[2])))*(float)rad2degree;
+  tangle[0] = atan(scaled[0]/(simu_sqrt(scaled[0]*scaled[0]+scaled[2]*scaled[2])))*(float)rad2degree;
 
-  if (angle>360)
+  angle[0] = Filter_gain*tangle[3]+(1-Filter_gain)*tangle[0];
+  angle[1] = Filter_gain*tangle[4]+(1-Filter_gain)*tangle[1];
+  angle[2] = Filter_gain*tangle[5]+(1-Filter_gain)*tangle[2];
+
+  last_count = current_count;
+
+  if (TIM6 -> CNT > 0xAAAA)
   {
-    angle -= 360;
-  }
-  else if (angle<-360)
-  {
-    angle += 360;
-  }
-
-  last_angle_speed = current_angle_speed;
-  last_count = TIM6 -> CNT;
-
-  if (last_count> 0xAAAA)
-  {
-    TIM6 -> CNT = 0;
+    TIM6 -> CNT = (TIM6 -> CNT)- last_count;
     last_count = TIM6 -> CNT;
   }
 }
---
+++ Code/src/angle.h
@@ -0,0 +1,16 @@
+#ifndef _ANGLE_H_
+#define _ANGLE_H_
+
+#include "mcu_lib.h"
+#define Filter_gain 0.95
+#define rad2degree 57.3
+
+float accel_scale_fact = 8192;
+float gyro_scale_fact = 65.5; // Scale factor variables
+int time_scale = 5000;
+float angle[3] = {0};
+
+void Angle_SetInitial();
+void MPU6050_UpdateAngle(float* angle);
+
+#endif
