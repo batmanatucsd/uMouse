924949d5b7f4f42d1418f07ce1c5a99d1f54151c
Yiyu Zhu <smallzzy@outlook.com>
2015-09-08 19:08:29 +0800

    refined delay

--- Code/src/mcu_delay.c
+++ Code/src/mcu_delay.c
@@ -1,23 +1,24 @@
 #include "mcu_delay.h"
 
+#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/cm3/systick.h>
 
-static u8  factor_us = 0;
-static u16 factor_ms = 0;
+static uint8_t  factor_us = 0;
+static uint16_t factor_ms = 0;
 
 void delay_init(void)
 {
     //set systick to AHB/8 = 72000000/8 = 9000000
     systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
-	factor_us = (u8)(rcc_ahb_frequency / (8*1000000));
-	factor_ms = (u16)factor_us*1000;
+	factor_us = (uint8_t)(rcc_ahb_frequency / (8*1000000));
+	factor_ms = (uint16_t)factor_us*1000;
 }
 
 //  note the range for us
 //  systick -> load is 24-bit reg
 //  so max delay = 0xffffff * 1000000 / factor_us (in us)
 //  for 72Mhz, max delay = 1864135 us
-void delay_us(u32 us)
+void delay_us(uint32_t us)
 {
     systick_set_reload(us*factor_us);// load time
 	systick_clear();               // clear counter
@@ -31,7 +32,7 @@ void delay_us(u32 us)
 //  systick -> load is 24-bit reg
 //  so max delay = 0xffffff * 1000 / factor_us (in ms)
 //  for 72Mhz, max delay = 1864 ms
-void delay_ms(u16 ms)
+void delay_ms(uint16_t ms)
 {
     systick_set_reload(ms*factor_ms);// load time
     systick_clear();               // clear counter
--- Code/src/mcu_delay.h
+++ Code/src/mcu_delay.h
@@ -1,8 +1,10 @@
 #ifndef _MCU_DELAY_H_
 #define _MCU_DELAY_H_
 
+#include <stdint.h>
+
 void delay_init(void);
-void delay_ms(u16 ms);
-void delay_us(u32 us);
+void delay_ms(uint16_t ms);
+void delay_us(uint32_t us);
 
 #endif
