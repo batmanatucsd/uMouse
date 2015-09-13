3df0024c91c481b196defa3e34fa5b1ed7b0f5a3
Yiyu Zhu <smallzzy@outlook.com>
2015-09-13 10:58:11 +0800

    new adc is ready
    first request the data and do other things and you can get the data back
    later by read it.

--- Code/src/mcu_adc.c
+++ Code/src/mcu_adc.c
@@ -12,6 +12,7 @@
 
 #include <stdint.h>
 
+static uint8_t data_ready = 0;
 uint16_t sensor_buffers[4];
 
 static void ADC_RCC_init(void)
@@ -21,8 +22,31 @@ static void ADC_RCC_init(void)
     rcc_set_adcpre(RCC_CFGR_ADCPRE_PCLK2_DIV6);
 
     //Enable ADC & GPIO
-    rcc_periph_clock_enable(REC_RCC_ADC | REC_RCC_PORT |
-        REC_RCC_DMA);
+    rcc_periph_clock_enable(REC_RCC_PORT | EMIT_RCC_PORT |
+        REC_RCC_DMA | REC_RCC_ADC);
+}
+
+static void ADC_GPIO_init(void)
+{
+    // set up receiver gpio
+    gpio_set_mode(REC_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG,
+        L_REC_PIN | LF_REC_PIN | RF_REC_PIN | R_REC_PIN);
+
+    // Set up emitter gpio
+    gpio_set_mode(EMIT_PORT, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL,
+        L_EMIT_PIN | LF_EMIT_PIN | RF_EMIT_PIN | R_EMIT_PIN);
+}
+
+static void ADC_NVIC_init(void)
+{
+    //divide Priority into 2 Preemption & 2 Sub
+    scb_set_priority_grouping(SCB_AIRCR_PRIGROUP_GROUP4_SUB4);
+
+    // Priority[0~255] = [7:4][3:0]
+    // [3:0] is ignored
+    // [7:4] is seperated into Preemption and sub according to scb_aircr
+    nvic_enable_irq(REC_DMA_ISR);
+    nvic_set_priority(REC_DMA_ISR, 0);
 }
 
 static void ADC_DMA_init(void)
@@ -55,18 +79,6 @@ static void ADC_DMA_init(void)
     dma_enable_channel(REC_DMA, REC_DMA_CHANNEL);
 }
 
-static void ADC_NVIC_init(void)
-{
-    //divide Priority into 2 Preemption & 2 Sub
-    scb_set_priority_grouping(SCB_AIRCR_PRIGROUP_GROUP4_SUB4);
-
-    // Priority[0~255] = [7:4][3:0]
-    // [3:0] is ignored
-    // [7:4] is seperated into Preemption and sub according to scb_aircr
-    nvic_enable_irq(NVIC_DMA1_CHANNEL1_IRQ);
-    nvic_set_priority(NVIC_DMA1_CHANNEL1_IRQ, 0);
-}
-
 void ADC_init(void)
 {
     // init adc clk & gpio clk
@@ -75,9 +87,7 @@ void ADC_init(void)
     /* Make sure the ADC doesn't run during config. */
     adc_off(REC_ADC);
 
-    // set up gpio
-    gpio_set_mode(REC_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG,
-        L_REC_PIN | LF_REC_PIN | RF_REC_PIN | R_REC_PIN);
+    ADC_GPIO_init();
 
     ADC_NVIC_init();
 
@@ -87,52 +97,88 @@ void ADC_init(void)
     // setup independent mode
     adc_set_dual_mode(ADC_CR1_DUALMOD_IND);
 
-    // scan regular channels once when requested
+    // scan regular channels only when requested
     adc_enable_scan_mode(REC_ADC);
     adc_set_single_conversion_mode(REC_ADC);
 
-    // will request so disable all trigger
+    // disable all external trigger
     adc_disable_external_trigger_regular(REC_ADC);
     adc_disable_external_trigger_injected(REC_ADC);
+
+    // set internal trigger mode : software start
+    ADC_CR2(REC_ADC) |= ADC_CR2_EXTSEL_SWSTART;
 
     // align to LSB
     adc_set_right_aligned(REC_ADC);
 
+    //set sample time
     adc_set_sample_time(REC_ADC, L_REC_ADC, REC_ADC_TSAMPLE);
     adc_set_sample_time(REC_ADC, LF_REC_ADC,REC_ADC_TSAMPLE);
     adc_set_sample_time(REC_ADC, RF_REC_ADC,REC_ADC_TSAMPLE);
     adc_set_sample_time(REC_ADC, R_REC_ADC, REC_ADC_TSAMPLE);
 
+    // set up regular group sequence
     uint8_t channel_group[] = {L_REC_ADC, LF_REC_ADC,
         RF_REC_ADC, R_REC_ADC};
 
+    // initial the sequence
     adc_set_regular_sequence(REC_ADC, 4, channel_group);
 
+    // power on adc
     adc_power_on(REC_ADC);
 
+    // delay to wait for stablize
     delay_ms(10);
 
+    // enable dma channel
     adc_enable_dma(REC_ADC);
 
+    // self calibration
     adc_reset_calibration(REC_ADC);
     adc_calibration(REC_ADC);
 }
 
 void ADC_request()
 {
-    GPIO_SetBits(EMITTER, R_EMITTER);
-    GPIO_SetBits(EMITTER, RF_EMITTER);
-    GPIO_SetBits(EMITTER, LF_EMITTER);
-    GPIO_SetBits(EMITTER, L_EMITTER);
+    // start receive
+    gpio_set(EMIT_PORT, L_EMIT_PIN);
+    gpio_set(EMIT_PORT, LF_EMIT_PIN);
+    gpio_set(EMIT_PORT, RF_EMIT_PIN);
+    gpio_set(EMIT_PORT, R_EMIT_PIN);
 
+    // wait for stablize
     delay_us(80);
-    ADC_SoftwareStartConvCmd(ADC3, ENABLE);
+
+    // start conversion
+    adc_start_conversion_regular(REC_ADC);
 }
 
 void ADC_read()
 {
-    GPIO_ResetBits(GPIOB, GPIO_Pin_12);
-    GPIO_ResetBits(GPIOB, GPIO_Pin_13);
-    GPIO_ResetBits(GPIOB, GPIO_Pin_14);
-    GPIO_ResetBits(GPIOB, GPIO_Pin_15);
+    while(!data_ready);
+
+    data_ready = 0;
+}
+
+void dma1_channel1_isr(void)
+{
+    if(dma_get_interrupt_flag(REC_DMA, REC_DMA_CHANNEL, DMA_TCIF))
+    {
+        // clear intertupt flag
+        dma_clear_interrupt_flags(REC_DMA, REC_DMA_CHANNEL, DMA_TCIF);
+
+        // data is ready;
+        data_ready = 1;
+
+        // close emitter
+        gpio_clear(EMIT_PORT, L_EMIT_PIN);
+        gpio_clear(EMIT_PORT, LF_EMIT_PIN);
+        gpio_clear(EMIT_PORT, RF_EMIT_PIN);
+        gpio_clear(EMIT_PORT, R_EMIT_PIN);
+    }
+    else
+    {
+        // stop if other interrupt took place
+        while(1);
+    }
 }
--- Code/src/mcu_adc.h
+++ Code/src/mcu_adc.h
@@ -11,15 +11,21 @@
 #define RF_REC_PIN  GPIO2
 #define R_REC_PIN   GPIO3
 
+#define EMIT_RCC_PORT   RCC_GPIOB
+#define EMIT_PORT       GPIOB
+
+#define L_EMIT_PIN      GPIO_15
+#define LF_EMIT_PIN     GPIO_14
+#define RF_EMIT_PIN     GPIO_13
+#define R_EMIT_PIN      GPIO_12
+
 #define REC_RCC_DMA RCC_DMA1
 #define REC_DMA     DMA1
-#define REC_DMA_CHANNEL 1
-
-#define REC_DMA_ISR NVIC_DMA1_CHANNEL1_IRQ
+#define REC_DMA_CHANNEL DMA_CHANNEL1
+#define REC_DMA_ISR     NVIC_DMA1_CHANNEL1_IRQ
 
 #define REC_RCC_ADC RCC_ADC1
-#define REC_ADC ADC1
-
+#define REC_ADC     ADC1
 #define REC_ADC_TSAMPLE ADC_SMPR_SMP_71DOT5CYC
 
 #define L_REC_ADC   ADC_CHANNEL10
